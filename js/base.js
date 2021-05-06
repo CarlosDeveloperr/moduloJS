53f6a93bc0371c116ee6ca7e91615b928b131055
CarlosDeveloperr <luis9620@hotmail.com>
2021-05-06 09:20:07 -0500

    Ejercicios  dia 3

--- js/base.js
+++ js/base.js
@@ -1,108 +1,70 @@
-console.log('Condicionales y Métodos de string')
+console.log('Metodos de sting y ciclos')
+
 
 
 /**
- * 
- * Pedir 3 numeros por le promt
- * Imprimir en consola cual es el numero mas grande de los 3
- *
+ * Pedir al usuario usuario un numero entre 1 y 10
+ * Imprimir la tabla de multiplicar de ese número
+ * p.ej. 
+ * 3 x 1 = 3
+ * 3 x 2 = 6
  */
-
-// const numberOne = parseInt(window.prompt('Dame un numero'))
-// const numberTwo = parseInt(window.prompt('Dame un numero'))
-// const numberThree = parseInt(window.prompt('Dame un numero'))
-// if (numberOne > numberTwo && numberOne > numberThree) {
-// 	console.log(`El número ${numberOne} es el mayor`)
-// } else if (numberTwo > numberOne && numberTwo > numberThree) {
-// 	console.log(`El número ${numberTwo} es el mayor`)
-// } else if (numberThree > numberOne && numberThree > numberTwo) {
-// 	console.log(`El número ${numberThree} es el mayor`)
-// } else {
-// 	console.log(`Todos los numeros son iguales`)
+// let numero= parseInt(window.prompt("Que numero deseas?"))
+// let i=1
+// console.log(`La tabla es del ${numero}\n`)
+// while(i<=10){
+//     let resultado=numero*i
+//    console.log(`${numero}*${i} = ${resultado}`);
+//    i++
 // }
 
-/**
- * 
- * Pedir al usuario 2 números (a y  b)
- * Comprobar si “a” es mayor o menor que “b”
- * Imprimir en consola el resultado
- * P.ej:  5 es menor que 6
- * 
- */
-
-// let numberA = parseInt(window.prompt('Dame el numero 1'))
-// let numberB = parseInt(window.prompt('Dame el numero 2'))
-// numberA > numberB ? console.log(`${a} es mayor que ${b}`) : console.log(`${a} es menor que ${b}`)
-
-
-/**
- * 
- * Pedir una calificación por promt
- * si es 100 -> A+
- * si es 90 -> A
- * si es 80 -> B
- * si es 70 -> C
- * si es 60 -> D
- * si es 50 -> E
- * si es 40 -> F
- * Imprimir en consola -> tu calificación es un A, b, c ...
- * 
- */
-
-// let score = parseInt(window.prompt('Cual fue la calificación de tu examen'))
-// switch (score) {
-// 	case 100:
-// 		console.log('tu califiación es un A+')
-// 		break
-// 	case 90:
-// 		console.log('tu califiación es un A')
-// 		break
-// 	case 80:
-// 		console.log('tu califiación es un B')
-// 		break
-// 	case 70:
-// 		console.log('tu califiación es un C')
-// 		break
-// 	case 60:
-// 		console.log('tu califiación es un D')
-// 		break
-// 	case 50:
-// 		console.log('tu califiación es un E')
-// 		break
-// 	default:
-// 		console.log('tu califiación es un F')
+// for(let i=1;i<=10;i++){
+//     let resultado=numero*i
+//     console.log(`${numero}*${i}= ${resultado}`);
 // }
 
 
-
+/**
+ * Pedir al usuario una oracion
+ * Imprimir un string con todas las vocales
+ * Imprimir un string con todas las consonantes
+ * 
+ */
+//  let oracion=window.prompt("Dame una oracion")
+//  let vocales=""
+//  let consonantes=""
+// //  carlos
+// for(i=0;i<=oracion.length-1;i++){
+//     if(oracion[i]!==' ' || oracion[i]!==',' || oracion[i]!=='.'){
+//         if(oracion[i]=='a' || oracion[i]=='e'|| oracion[i]=='i' || oracion[i]=='o' || oracion[i]=='u' 
+//             || oracion[i]=='A' || oracion[i]=='E'|| oracion[i]=='I' || oracion[i]=='O' || oracion[i]=='U' )
+//         {
+//             vocales+=oracion[i]
+            
+//         }
+//         else{
+//             consonantes+=oracion[i]
+//         }
+//     }
+//  }
+//  console.log(vocales)
+//  console.log(consonantes)
 
 /**
- * 
- * Propiedades de string
- * Métodos de string
- *
+ * Pedir el usuario un numero entre 10 y 100
+ * Imprimir todos los numeros pares que existen entre 1 y ese numero
  */
 
-// Propiedad length
-// sintaxis "string".length
-
-let saludo = "Hola mundo"
-console.log(saludo.length)
-
-// Métodos
-
-// string.split(separator, limit)
-let resulSplit = saludo.split(' ')
-console.log(resulSplit)
-// -> ["Hola", "mundo"]
-
-// string.concat(string1, string2, ..., stringX)
-const str1 = "Hola "
-const str2 = "Mundo!"
-let resConcat = str1.concat(str2)
-// -> "Hola Mundo"
-
-// string.replace(searchvalue, newvalue)
-var strToReplace = "Visit México!"
-var resReplace = strToReplace.replace("México", "Kodemia")
-// -> "Visit Kodemia!"
+let num=parseInt(window.prompt("Dame el numero que quieres saber sus pares entre 10-100"))
+let i=1
+while(i<=num){
+    if(i%2==0){
+    console.log(` El numero ${i} es par`)
+             }
+    i++
+}
+// for(i=1;i<=num;i++){
+//     if(i%2==0){
+//         console.log(` El numero ${i} es par`)
+//     }
+// }

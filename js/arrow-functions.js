15940ecd4f5777d6196398e5a0ee6beaa1b8bdcf
CarlosDeveloperr <luis9620@hotmail.com>
2021-05-12 00:07:58 -0500

    Ejercicios  dia 6

--- js/arrow-functions.js
+++ js/arrow-functions.js
@@ -1,47 +1,57 @@
-let dineroEnCuenta=2000
- 
- /*funciones como arrow functions*/
-  const consultarDinero = ()=>console.log(`El dinero que tienes en la cuenta en este momento es :${dineroEnCuenta}`)
-  
+let dineroEnCuenta = 2000;
 
- const retirarDinero = (dineroActual,dineroRetiro) =>{
-    if(dineroRetiro>0){
-      if(dineroActual>= dineroRetiro)
-      {
-              let valor=dineroEnCuenta-dineroRetiro
-              return valor
-      }else{
-        console.log("no tienes dinero suficiente")
-      }
-    }else{
-      console.log("Numero mayor que 0 por favor")
+/*funciones como arrow functions*/
+const consultarDinero = () =>
+  console.log(
+    `El dinero que tienes en la cuenta en este momento es :${dineroEnCuenta}`
+  );
+
+const retirarDinero = (dineroActual, dineroRetiro) => {
+  if (dineroRetiro > 0) {
+    if (dineroActual >= dineroRetiro) {
+      let valor = dineroEnCuenta - dineroRetiro;
+      return valor;
+    } else {
+      console.log("no tienes dinero suficiente");
     }
-   
+  } else {
+    console.log("Numero mayor que 0 por favor");
   }
+};
 
-const depositarDinero =(dineroActual,dineroDepositar)=>dineroDepositar>0 ? dineroActual+dineroDepositar : console.log('No se realizo deposito')
-  
- const transferencia = (saldo,cantidad,cuenta="1234") => {
-   cantidad>0 ? 
-        cuenta ==="1234" ?  dineroEnCuenta-=cantidad:console.log("cuenta invalida")
-   : console.log("Dame una cantidad valida...")
+const depositarDinero = (dineroActual, dineroDepositar) =>
+  dineroDepositar > 0
+    ? dineroActual + dineroDepositar
+    : console.log("No se realizo deposito");
 
- }
- 
-        
+const transferencia = (saldo, cantidad, cuenta = "1234") => {
+  return cantidad > 0
+    ? cuenta === "1234"
+      ? saldo - cantidad
+      : console.log("cuenta invalida")
+    : console.log("Dame una cantidad valida...");
+};
+// const transferencia = (saldo, cantidad, cuenta = "1234") =>
+//      cantidad > 0
+//       ? cuenta === "1234"
+//         ? saldo - cantidad
+//         : console.log("cuenta invalida")
+//       : console.log("Dame una cantidad valida...");
+//
+
 /*Saldo inicial*/
-consultarDinero(dineroEnCuenta) 
-/* 1. Depositar 2000*/ 
-dineroEnCuenta=depositarDinero(dineroEnCuenta,2000)
-/* 2. Retirar 450*/ 
-dineroEnCuenta=retirarDinero(dineroEnCuenta,450)
-/* 3. Consultar saldo*/ 
-consultarDinero(dineroEnCuenta)
-/* 4. Retirar 400*/ 
-dineroEnCuenta=retirarDinero(dineroEnCuenta,400)
+consultarDinero(dineroEnCuenta);
+/* 1. Depositar 2000*/
+dineroEnCuenta = depositarDinero(dineroEnCuenta, 2000);
+/* 2. Retirar 450*/
+dineroEnCuenta = retirarDinero(dineroEnCuenta, 450);
+/* 3. Consultar saldo*/
+consultarDinero(dineroEnCuenta);
+/* 4. Retirar 400*/
+dineroEnCuenta = retirarDinero(dineroEnCuenta, 400);
 /*5. Consultar saldo*/
-consultarDinero(dineroEnCuenta)
-/*6. Transferencia */ console.log(dineroEnCuenta)
-transferencia(dineroEnCuenta,2000)
+consultarDinero(dineroEnCuenta);
+/*6. Transferencia */
+dineroEnCuenta = transferencia(dineroEnCuenta, 200);
 /*7. Consultar saldo*/
-consultarDinero(dineroEnCuenta)
+consultarDinero(dineroEnCuenta);
